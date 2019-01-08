54dbeae6b4a528486bebb6bfbc73bfad0e66c9fa
Craigthomaskey <craigthomaskey@gmail.com>
2019-01-08 15:16:22 -0600

    Testing data managment, added json control for data. (Why reinvent the wheel?)

--- AlphaVantage - HistoricalData/Form1.Designer.cs
+++ AlphaVantage - HistoricalData/Form1.Designer.cs
@@ -28,21 +28,64 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.chart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.chart)).BeginInit();
             this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(208, 426);
+            this.textBox1.TabIndex = 0;
+            // 
+            // chart
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart.ChartAreas.Add(chartArea1);
+            this.chart.Location = new System.Drawing.Point(226, 12);
+            this.chart.Name = "chart";
+            this.chart.Size = new System.Drawing.Size(562, 397);
+            this.chart.TabIndex = 1;
+            this.chart.Text = "chart1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(713, 415);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "GetData";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_ClickAsync);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.chart);
+            this.Controls.Add(this.textBox1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- AlphaVantage - HistoricalData/Form1.cs
+++ AlphaVantage - HistoricalData/Form1.cs
@@ -1,25 +1,115 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using AlphaVantage.Net.Stocks;
+using AlphaVantage.Net.Stocks.TimeSeries;
+using System.Windows.Forms.DataVisualization.Charting;
 
 namespace AlphaVantage___HistoricalData
 {
     public partial class Form1 : Form
     {
-        public Form1()
-        {
-            InitializeComponent();
-        }
+
+        string APIKey = "XZG6SFKFFJUEOHBX";
+
+        public Form1() =>                   InitializeComponent();
+        
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            ChartSetUp();         
+        }
+
+        void ChartSetUp()
+        {
+            Series series = new Series("Price");            series.ChartType = SeriesChartType.Line;            series.BorderWidth = 3;
+            series.Color = Color.Gray;            series.IsVisibleInLegend = false;           chart.Series.Add(series);
+            chart.ChartAreas[0].AxisX.MajorGrid.Enabled = false;           chart.ChartAreas[0].AxisY.MajorGrid.Enabled = false;
+            chart.ChartAreas[0].AxisX.LabelStyle.Enabled = true;            chart.ChartAreas[0].AxisX.MajorTickMark.Enabled = true;
+            chart.ChartAreas[0].AxisX.IsMarginVisible = false;            chart.ChartAreas[0].AxisY.IsMarginVisible = false;
+        }
+
+
+
+
+        private async void button1_ClickAsync(object sender, EventArgs e)
+        {
+            await GetIntraData(); AddChartData();
+        }
+
+
+        Dictionary<DateTime, decimal[]> IntraDataDict = new Dictionary<DateTime, decimal[]>();
+        async Task GetIntraData()
+        {
+            IntraDataDict.Clear();
+            var client = new AlphaVantageStocksClient(APIKey);
+            StockTimeSeries timeSeries = await client.RequestIntradayTimeSeriesAsync("GE", IntradayInterval.FiveMin, TimeSeriesSize.Compact);    // client.RequestDailyTimeSeriesAsync("GE", TimeSeriesSize.Compact, false);
+            foreach (var dataPoint in timeSeries.DataPoints)
+            {
+                if (DateTime.Now.Date == dataPoint.Time.Date)
+                {
+                    decimal[] tempDecimal = new decimal[4];
+                    tempDecimal[0] = dataPoint.HighestPrice;
+                    tempDecimal[1] = dataPoint.LowestPrice;
+                    tempDecimal[2] = dataPoint.OpeningPrice;
+                    tempDecimal[3] = dataPoint.ClosingPrice;
+                    IntraDataDict[dataPoint.Time] = tempDecimal;
+                }
+            }
+        }
+
+
+
+        void AddChartData()
+        {
+            chart.Series["Price"].Points.Clear();
+            double low = 0; double high = 0;
+            for (int i = IntraDataDict.Count-1; i >= 0; i--)
+            {            
+                double close = Convert.ToDouble(IntraDataDict.ElementAt(i).Value[3]);
+                chart.Series["Price"].Points.AddXY(IntraDataDict.ElementAt(i).Key.ToString("hh:mm:ss"), close);
+                if (low > close || low == 0) low = close; if (high < close) high = close;
+                chart.ChartAreas[0].AxisY.Maximum = (high + .25); chart.ChartAreas[0].AxisY.Minimum = (low - .25);
+
+                textBox1.Text = IntraDataDict.ElementAt(i).Key.ToString("hh:mm:ss") + " : " + IntraDataDict.ElementAt(i).Value[3] + Environment.NewLine + textBox1.Text;
+            }
+        }
+
+
+
+
+        void BuildMovingAverage(int intervals)
+        {
+            foreach (var item in )
+            {
+
+            }
+
+
+
 
         }
+
+
+
+
+
+
+
+
+
+
+
     }
 }
